4c16a5b987a9cc80a71ba56b07eab6f63e16fa48
Felipe Kreulich <felipek2002k@gmail.com>
2022-07-12 11:42:38 -0400

    master

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './footer.css';
+import { BsFacebook, BsGithub } from 'react-icons/bs';
+import { AiFillInstagram } from 'react-icons/ai';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>FelipeKreulich</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><BsFacebook/></a>
+        <a href="https://instagram.com"><AiFillInstagram/></a>
+        <a href="https://github.com"><BsGithub/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Felipe Kreulich. Alls rights reserved.</small>
+      </div>
+    </footer>
   )
   
 }
